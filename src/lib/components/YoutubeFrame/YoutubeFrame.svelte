81ff3aa0fb00e30d73223405a2e676f990bb2f82
yuma0306 <yuma0306895@gmail.com>
2026-06-07 23:21:54 +0700

    修正

--- src/lib/components/YoutubeFrame/YoutubeFrame.svelte
+++ src/lib/components/YoutubeFrame/YoutubeFrame.svelte
@@ -4,6 +4,9 @@ import type { Situation } from '$lib/types';
 let { youtubeId }: { youtubeId: Situation['youtubeId'] } = $props();
 </script>
 
+<a class="link" href={`https://youtu.be/${youtubeId}`} target="_blank" rel="noopener noreferrer">
+	出典（埋め込み動画参照）: @PHOTOGLISH
+</a>
 <iframe
 	width="100%"
 	height="100%"
@@ -30,5 +33,35 @@ let { youtubeId }: { youtubeId: Situation['youtubeId'] } = $props();
 	margin-inline: auto;
 	display: block;
 	aspect-ratio: 16 / 9;
+}
+.link {
+	display: block;
+	width: fit-content;
+	margin-inline: auto;
+	text-decoration: underline;
+	color: var(--color-primary);
+	transition: opacity var(--transition);
+	@media (hover: hover) {
+		&:hover {
+			opacity: var(--opacity);
+		}
+	}
+	&:active {
+		opacity: var(--opacity);
+	}
+}
+@media (min-width: 640px) {
+	.link {
+		font-size: calc(var(--font-size-2) * 1px);
+		transition: opacity var(--transition);
+		&:hover {
+			opacity: var(--opacity);
+		}
+	}
+}
+@media (max-width: 639px) {
+	.link {
+		font-size: calc(var(--calc-sp) * var(--font-size-2));
+	}
 }
 </style>
